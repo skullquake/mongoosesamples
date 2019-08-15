b1c23ffcd683d30a6ac1210754c453fa54f721e8
skullquake <ockert8080@gmail.com>
2019-08-15 07:59:31 +0200

    client

--- src/000_client/src/a.c
+++ src/000_client/src/a.c
@@ -1,7 +1,5 @@
 #include<mongoose.h>
 static int s_exit_flag=0;
-static int s_show_headers=0;
-static const char *s_show_headers_opt="--show-headers";
 static void ev_handler(struct mg_connection *nc,int ev,void *ev_data){
 	struct http_message *hm=(struct http_message *)ev_data;
 	switch(ev) {
@@ -19,13 +17,9 @@ static void ev_handler(struct mg_connection *nc,int ev,void *ev_data){
 			break;
 		case MG_EV_HTTP_REPLY:
 			nc->flags|=MG_F_CLOSE_IMMEDIATELY;
-			if(s_show_headers){
-				printf("here");
-				fwrite(hm->message.p,1,hm->message.len,stdout);
-			} else {
-				printf("%d",hm->body.len);
-				fwrite(hm->body.p,1,hm->body.len,stdout);
-			}
+			fwrite(hm->message.p,1,hm->message.len,stdout);
+			printf("%d",hm->body.len);
+			fwrite(hm->body.p,1,hm->body.len,stdout);
 			putchar('\n');
 			s_exit_flag=1;
 			break;
@@ -41,9 +35,8 @@ static void ev_handler(struct mg_connection *nc,int ev,void *ev_data){
 }
 int main(int argc,char *argv[]){
 	struct mg_mgr mgr;
-	int i;
 	mg_mgr_init(&mgr,NULL);
-	mg_connect_http(&mgr,ev_handler,"http://127.0.0.1:8080/README.md",NULL,NULL);
+	mg_connect_http(&mgr,ev_handler,"http://co.za/whois.shtml",NULL,NULL);
 	while(s_exit_flag==0){
 		mg_mgr_poll(&mgr,1000);
 	}
